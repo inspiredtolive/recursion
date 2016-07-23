d36169a406083d53ffc4030c794f045b29c90ea5
Marco Chan <inspiredtolive0@gmail.com>
2016-07-22 19:51:34 -0700

    Implement stringifyJSON recursively

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,39 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+  if (Array.isArray(obj)) {
+    // Stringify all array elements and put into elements variable
+    var elements = obj.reduce(function (p, c) {
+      var ele = stringifyJSON(c);
+      if (p.length > 0 && ele.length > 0) {
+        return p + ',' + ele;
+      }
+      return ele;
+    }, '');
+    return '[' + elements + ']';
+  }
+  if (typeof obj === 'object' && obj !== null) {
+    // Stringify all keys and their values
+    var keyPairs = Object.keys(obj).reduce(function (p, c) {
+      var value = stringifyJSON(obj[c]);
+      var keyPair;
+      if (value === '') { 
+        keyPair = ''; 
+      } else {
+        keyPair = stringifyJSON(c) + ':' + value;
+      }
+      if (p.length > 0 && keyPair.length > 0) {
+        return p + ',' + keyPair;
+      }
+      return keyPair;
+    }, '');
+    return '{' + keyPairs + '}';
+  }
+  if (obj === undefined || typeof obj === 'function') {
+    return '';
+  }
+  return '' + obj;
 };
